5facdc0e9c92c29633a937b1e457d6acd4297165
Sam Noh <samnoh21@outlook.com>
2019-06-18 16:04:51 +1200

    fix filesync bug

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
 #!/usr/bin/env node
 const path = require('path');
-const program = require('commander');
 require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
+// const program = require('commander');
 
 const processComments = require('./util/processComments');
 const pickWinners = require('./util/pickWinners');
 const writeFile = require('./util/writeFile');
 
-program
-	.version('0.0.1', '-v, --version')
-	.action(() => {
-		processComments()
-			.then(candidates => {
-				if (process.argv[3]) {
-					return pickWinners(candidates);
-				}
-				return { winners: candidates, candidateSize: candidates.size };
-			})
-			.then(winners => writeFile(winners))
-			.then(message => {
-				console.log(message);
-				process.exit(0);
-			})
-			.catch(error => {
-				console.error(error);
-			});
+processComments()
+	.then(candidates => {
+		if (process.argv[3]) {
+			return pickWinners(candidates);
+		}
+		return { winners: candidates, candidateSize: candidates.size };
 	})
-	.parse(process.argv);
+	.then(winners => writeFile(winners))
+	.then(message => {
+		console.log(message + '\nbye');
+		process.exit(0);
+	})
+	.catch(error => {
+		console.error(error);
+	});
 
 setTimeout(() => {
 	console.error('Error: timeout');
--- src/util/writeFile.js
+++ src/util/writeFile.js
@@ -1,22 +1,33 @@
 const fs = require('fs');
-const chalk = require('chalk');
+const path = require('path');
 
-const writeStream = fs.createWriteStream('./comments.txt');
+const filepath = path.join(process.cwd(), `comments-${Date.now()}.txt`);
 
 const writeFile = ({ winners, candidateSize }) => {
-	writeStream.write(
-		`${winners.size} winner${winners.size == 1 ? '' : 's'} of ${candidateSize} candidate${
-			candidateSize == 1 ? '' : 's'
-		}\n\n-------------------------\n\n`
-	);
+	if (process.argv[3]) {
+		fs.writeFileSync(
+			filepath,
+			`${winners.size} winner${winners.size == 1 ? '' : 's'} of ${candidateSize} candidate${
+				candidateSize == 1 ? '' : 's'
+			}\n\n-------------------------\n\n`,
+			err => {
+				if (err) Promise.reject('Error: writeFileSync');
+			}
+		);
+	}
 
 	winners.forEach(w => {
-		writeStream.write(
-			`#${w.id} ${w.author}\n\n${w.email}\n\n${w.comment.trim()}\n\n-------------------------\n\n`
+		fs.appendFileSync(
+			filepath,
+			`#${w.id} ${w.author}\n\n${
+				w.email ? w.email + '\n\n' : ''
+			}${w.comment.trim()}\n\n-------------------------\n\n`,
+			err => {
+				if (err) Promise.reject('Error: appendFile');
+			}
 		);
 	});
 
-	writeStream.close();
 	return Promise.resolve('Done!');
 };
 
