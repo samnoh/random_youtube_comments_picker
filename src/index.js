1f7f3e9637d4c96d9d80b60e5b8c4ac27aebeaa9
Sam Noh <samnoh21@outlook.com>
2019-06-23 16:34:33 +1200

    clean up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
-// const program = require('commander');
 
 const processComments = require('./util/processComments');
 const pickWinners = require('./util/pickWinners');
